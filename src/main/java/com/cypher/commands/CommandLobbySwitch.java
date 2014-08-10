b43bf84a3d6ae7eafd54eb275d01b697112c305b
Derek <derekbking@gmail.com>
2014-08-09 22:28:57 -0500

    Added /lobbyswitch list Added command auto complete

--- src/main/java/com/cypher/commands/CommandLobbySwitch.java
+++ src/main/java/com/cypher/commands/CommandLobbySwitch.java
@@ -6,9 +6,17 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CommandLobbySwitch implements TabExecutor {
+
+    private final List<String> arguments = new ArrayList<String>() {
+        {
+            add("list");
+            add("version");
+        }
+    };
 
     private String PREFIX = "\u00bb ";
 
@@ -20,23 +28,50 @@ public class CommandLobbySwitch implements TabExecutor {
         switch (args.length) {
             case 1:
                 switch (args[0].toLowerCase()) {
+                    case "list":
+                    case "l":
+                        commandSender.sendMessage(ChatColor.DARK_RED + PREFIX + ChatColor.RED + ChatColor.BOLD + "Server list");
+                        for (String string : (ArrayList<String>) LobbySwitch.p.getFileConfig().getList("Servers")) {
+                            String[] split = string.split(":");
+                            commandSender.sendMessage("  " + ChatColor.DARK_RED + PREFIX + ChatColor.GRAY + split[2]);
+                            commandSender.sendMessage("    " + ChatColor.DARK_RED + PREFIX + ChatColor.RED + "ItemStack: " + ChatColor.GRAY + split[0]);
+                            commandSender.sendMessage("    " + ChatColor.DARK_RED + PREFIX + ChatColor.RED + "Amount: " + ChatColor.GRAY + split[1]);
+                            commandSender.sendMessage("    " + ChatColor.DARK_RED + PREFIX + ChatColor.RED + "Color: " + ChatColor.GRAY + ChatColor.getByChar(split[4]).name());
+                            commandSender.sendMessage("    " + ChatColor.DARK_RED + PREFIX + ChatColor.RED + "Slot: " + ChatColor.GRAY + split[5]);
+                        }
+                        return true;
                     case "version":
                     case "v":
                         commandSender.sendMessage(ChatColor.DARK_RED + PREFIX + ChatColor.RED + "Lobby Switch version " + ChatColor.GRAY + LobbySwitch.p.getDescription().getVersion() + ChatColor.RED + ".");
                         return true;
                     default:
                         commandSender.sendMessage(ChatColor.DARK_RED + PREFIX + ChatColor.RED + ChatColor.BOLD + "Invalid command format");
+                        commandSender.sendMessage(ChatColor.DARK_RED + PREFIX + ChatColor.RED + "/lobbyswitch <list:l>");
                         commandSender.sendMessage(ChatColor.DARK_RED + PREFIX + ChatColor.RED + "/lobbyswitch <version:v>");
                 }
             default:
                 commandSender.sendMessage(ChatColor.DARK_RED + PREFIX + ChatColor.RED + ChatColor.BOLD + "Invalid command format");
+                commandSender.sendMessage(ChatColor.DARK_RED + PREFIX + ChatColor.RED + "/lobbyswitch <list:l>");
                 commandSender.sendMessage(ChatColor.DARK_RED + PREFIX + ChatColor.RED + "/lobbyswitch <version:v>");
         }
         return true;
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-        return null;
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String alias, String[] args) {
+        if (args.length > 1 || args.length == 0) {
+            return new ArrayList<String>();
+        }
+
+        List<String> matches = new ArrayList<String>();
+        if (args.length == 1) {
+            String search = args[0].toLowerCase();
+            for (String argument : arguments) {
+                if (argument.toLowerCase().startsWith(search)) {
+                    matches.add(argument);
+                }
+            }
+        }
+        return matches;
     }
 }
