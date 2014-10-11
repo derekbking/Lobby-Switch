ac546fe9eadd5bda22ec5112e47d4a94d1df8c87
Derek <derekbking@gmail.com>
2014-10-10 22:02:51 -0500

    Check the inventory name before switching servers

--- src/main/java/com/lobbyswitch/listeners/CypherInventoryListener.java
+++ src/main/java/com/lobbyswitch/listeners/CypherInventoryListener.java
@@ -4,6 +4,7 @@ import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import com.lobbyswitch.LobbySwitch;
 import com.lobbyswitch.ServerItem;
+import com.lobbyswitch.config.ConfigPaths;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,21 +20,23 @@ public class CypherInventoryListener implements Listener {
     @EventHandler
     public void onInventoryClickEvent(InventoryClickEvent event) {
         if (event.getClickedInventory() == event.getWhoClicked().getOpenInventory().getTopInventory()) {
-            if (event.getInventory().getSize() >= event.getSlot() && event.getSlot() >= 0) {
-                ItemStack itemStack = event.getInventory().getItem(event.getSlot());
-                if (itemStack != null) {
-                    if (event.getWhoClicked() instanceof Player) {
-                        for (String string : LobbySwitch.p.getConfigManager().getSlots()) {
-                            if (event.getSlot() == Integer.parseInt(string) - 1) {
-                                Player player = (Player) event.getWhoClicked();
-                                ServerItem serverItem = LobbySwitch.p.getConfigManager().getServerItem(Integer.parseInt(string));
+            if (event.getClickedInventory().getName().equals(LobbySwitch.p.getConfig().getString(ConfigPaths.INVENTORY_NAME))) {
+                if (event.getInventory().getSize() >= event.getSlot() && event.getSlot() >= 0) {
+                    ItemStack itemStack = event.getInventory().getItem(event.getSlot());
+                    if (itemStack != null) {
+                        if (event.getWhoClicked() instanceof Player) {
+                            for (String string : LobbySwitch.p.getConfigManager().getSlots()) {
+                                if (event.getSlot() == Integer.parseInt(string) - 1) {
+                                    Player player = (Player) event.getWhoClicked();
+                                    ServerItem serverItem = LobbySwitch.p.getConfigManager().getServerItem(Integer.parseInt(string));
 
-                                ByteArrayDataOutput byteArrayDataOutput = ByteStreams.newDataOutput();
+                                    ByteArrayDataOutput byteArrayDataOutput = ByteStreams.newDataOutput();
 
-                                byteArrayDataOutput.writeUTF("Connect");
-                                byteArrayDataOutput.writeUTF(serverItem.getTargetServer());
-                                player.sendPluginMessage(LobbySwitch.p, "BungeeCord", byteArrayDataOutput.toByteArray());
-                                event.setCancelled(true);
+                                    byteArrayDataOutput.writeUTF("Connect");
+                                    byteArrayDataOutput.writeUTF(serverItem.getTargetServer());
+                                    player.sendPluginMessage(LobbySwitch.p, "BungeeCord", byteArrayDataOutput.toByteArray());
+                                    event.setCancelled(true);
+                                }
                             }
                         }
                     }
